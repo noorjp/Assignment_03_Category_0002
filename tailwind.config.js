ba8c358976434457cf45f092be79cd620e9a166d
Md Noor hossain <hossainmdnoor7788@gmail.com>
2025-02-09 18:48:34 +0900

    Services updated

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            Text:"#111111",
+            Secoundry:"#EFEBE3",
+            Accent: "#179800", 
+          },
+      },
+      
+    },
+    plugins: [],
+  }
